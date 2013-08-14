e90fee8ec364db65af15defe4208445efd38ffee
toshiotm <toshi.otm@gmail.com>
2013-08-15 00:49:05 +0900

    added production code.

--- lib/tddbc.rb
+++ lib/tddbc.rb
@@ -1,12 +1,20 @@
 
 class Vender
+	
+  def initialize
+    @charge = 0
+  end
+
   def insert(money)
+  	@charge += money
   end
 
   def total
+  	@charge
   end
 
   def pay_back
+  	@charge
   end
 end
 
