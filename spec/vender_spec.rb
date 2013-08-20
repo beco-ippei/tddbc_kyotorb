e47611121456a603c90c3eb819cf37a9ae5fb6e8
beco-ippei <beco.ippei@gmail.com>
2013-08-21 01:02:15 +0900

    refactor spec

--- spec/vender_spec.rb
+++ spec/vender_spec.rb
@@ -1,44 +1,59 @@
 require 'vender'
 
-describe 'tddbc' do
-  before do
-    @vender = Vender.new
-  end
+def money_varieties
+  [10, 50, 100, 500, 1000]
+end
+
+describe Vender do
+  let(:vender) { Vender.new }
+
+  #XXX may be, this case is not nessesary.
+  it { expect(vender.instance_variable_get :@charge).to eq 0 }
 
   describe '#insert' do
-    subject {@vender.insert}
-    [10, 50, 100, 500, 1000].each do |fee|
-      it "not expect error on #{fee}" do
-        expect { fee }.not_to raise_error
+    subject { vender.insert }
+
+    money_varieties.each do |fee|
+      context "when #{fee}yen" do
+        it { expect { fee }.not_to raise_error }
       end
     end
 
-    it 'insert any times ' do
+    it 'can any times' do
       3.times.each do
         expect { 10 }.not_to raise_error
       end
     end
   end
 
+  shared_examples_for 'insert money variations' do
+    before do
+      moneis.each {|fee| vender.insert fee }
+    end
+    let(:total) { moneis.inject {|sum, i| sum+=i } }
+
+    context 'when insert all type and once' do
+      let!(:moneis) { money_varieties }
+      it { should eq total }
+    end
+
+    money_varieties.reverse.each_with_index do |fee, idx|
+      context "when insert #{fee}yen #{idx} times" do
+        let!(:moneis) { Array.new idx, fee }
+        it { should eq fee*idx }
+      end
+    end
+  end
+
   describe '#total' do
-    it 'can calcurate total charge' do
-
-      @vender.insert 10
-      @vender.insert 10
-      @vender.insert 100
-
-      expect(@vender.total).to eq 120
+    it_behaves_like 'insert money variations' do
+      subject { vender.total }
     end
   end
 
   describe '#pay_back' do
-    it 'return ...' do
-
-      @vender.insert 10
-      @vender.insert 10
-      @vender.insert 100
-
-      expect(@vender.pay_back).to eq 120
+    it_behaves_like 'insert money variations' do
+      subject { vender.pay_back }
     end
   end
 end
